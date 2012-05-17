7859147d0a0690d2f8fa12ff20c8344ba6936c77
Thomas Klemm <mail@tklemm.eu>
2012-05-17 22:14:44 +0200

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailForm::Application.config.secret_token = '78b147ae5d9fb7fe0236b532bf5a88365f085fef80a609c001fe044b7f556bb306f2e98c97d11ee41548a4bf601ef266bbf95623fd5a9680d1fea52bc5aafe9c'
